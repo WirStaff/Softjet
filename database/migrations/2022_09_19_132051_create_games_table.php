823c3e3029c36cfae0eb55d823e1f1086a1f82c6
Wirstaff <wirstaffdev@gmail.com>
2022-09-19 16:24:38 +0300

    migrations

--- database/migrations/2022_09_19_132051_create_games_table.php
+++ database/migrations/2022_09_19_132051_create_games_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->text('description');
             $table->timestamps();
         });
     }
