4c131484a429e8b5669db132046a1b3f978e786d
Wirstaff <wirstaffdev@gmail.com>
2022-09-19 16:50:26 +0300

    Create Base Repository

---
+++ app/Repositories/Repository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Repository
+{
+    /** @var Model $model */
+    protected $model;
+
+    public function getAll()
+    {
+        return app($this->model)::all();
+    }
+
+    public function getTableName()
+    {
+        return app($this->model)->getTable();
+    }
+
+    public function find(int $id)
+    {
+        $items = app($this->model)::query()->where('id', $id)->get(['*']);
+
+        return $items ? current(current($items)) : null;
+    }
+}
