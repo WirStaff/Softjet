173adad9651c6cb47f5602a545728834d34cef9f
Wirstaff <wirstaffdev@gmail.com>
2022-09-19 16:50:00 +0300

    Create Models

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Game extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function genres(): BelongsToMany
+    {
+        return $this->belongsToMany(Genre::class);
+    }
 }
